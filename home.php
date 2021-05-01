417b91244fdbe209a42092850cbde9ba082a5358
bethelthomas <bethelthomas55@gmail.com>
2021-05-01 13:41:04 +0530

    website files

---
+++ home.php
@@ -0,0 +1,311 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!--favicon-->
+        <link rel="shortcut icon" href="favicon.png" type="image/icon">
+        <link rel="icon" href="favicon.png" type="image/icon">
+        <title>XIE Campus online automation</title>
+        <!-- Bootstrap Core CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+        <!-- Footer -->
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+        <!-- Custom Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+        rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic'
+        rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+        <!-- Plugin CSS -->
+        <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="css/creative.css" type="text/css">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    
+    <body id="page-top">
+
+
+        <nav id="mainNav" class="navbar navbar-default navbar-fixed-top" style="background:black">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand page-scroll" href="http://www.cittumkur.org">Xavier Institute Of Engineering</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a class="page-scroll" href="../Profilers/SProfile/index.php">Student Login</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="../Profilers/HODProfile/index.php">HOD Login</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="../Profilers/PProfile/index.php">Placement Login</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="../Profilers/PriProfile/index.php">Administrative Login</a>
+                        </li>
+                    </ul>
+                
+            </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+        <header>
+            <div class="header-content">
+                <div class="header-content-inner">
+                    <h1 style="color:red";><mark>XIE CAMPUS ONLINE AUTOMATION</mark></h1>
+                    <hr>
+                                       <a href="../Drives/index.php" class="btn btn-primary btn-xl page-scroll">Current Drives</a>
+                </div>
+            </div>
+        </header>
+
+
+        <section class="course" id="course">
+
+<h1 class="heading">OUR POPULAR COURSES</h1>    
+
+
+        <div class="box-container">
+
+
+<div class="box">
+    <img src="images/c++.jpeg" alt="">
+    <h3 class="price">$50</h3>
+    <div class="content">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half"></i>
+        </div>
+        <a href="#" class="title">Learn C++</a>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, beatae. Modi quos excepturi id quibusdam? Molestiae quis nihil non debitis!</p>
+        <div class="info">
+            <h3> <i class="far fa-clock"></i> 2 hours </h3>
+            <h3> <i class="far fa-calendar-alt"></i> 6 months </h3>
+            <h3> <i class="fas fa-book"></i> 12 modules </h3>
+        </div>
+    </div>
+</div>
+
+<div class="box">
+    <img src="images/java.jpeg" alt="">
+    <h3 class="price">$50</h3>
+    <div class="content">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half"></i>
+        </div>
+        <a href="#" class="title">Getting started with Java</a>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, beatae. Modi quos excepturi id quibusdam? Molestiae quis nihil non debitis!</p>
+        <div class="info">
+            <h3> <i class="far fa-clock"></i> 2 hours </h3>
+            <h3> <i class="far fa-calendar-alt"></i> 6 months </h3>
+            <h3> <i class="fas fa-book"></i> 12 modules </h3>
+        </div>
+    </div>
+</div>
+
+<div class="box">
+    <img src="images/python.jpeg" alt="">
+    <h3 class="price">$50</h3>
+    <div class="content">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half"></i>
+        </div>
+        <a href="#" class="title">Python Programming</a>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, beatae. Modi quos excepturi id quibusdam? Molestiae quis nihil non debitis!</p>
+        <div class="info">
+            <h3> <i class="far fa-clock"></i> 2 hours </h3>
+            <h3> <i class="far fa-calendar-alt"></i> 6 months </h3>
+            <h3> <i class="fas fa-book"></i> 12 modules </h3>
+        </div>
+    </div>
+</div>
+
+<div class="box">
+    <img src="images/dbms.jpeg" alt="">
+    <h3 class="price">$50</h3>
+    <div class="content">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half"></i>
+        </div>
+        <a href="#" class="title">Advanced Database</a>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, beatae. Modi quos excepturi id quibusdam? Molestiae quis nihil non debitis!</p>
+        <div class="info">
+            <h3> <i class="far fa-clock"></i> 2 hours </h3>
+            <h3> <i class="far fa-calendar-alt"></i> 6 months </h3>
+            <h3> <i class="fas fa-book"></i> 12 modules </h3>
+        </div>
+    </div>
+</div>
+
+<div class="box">
+    <img src="images/cyber.jpeg" alt="">
+    <h3 class="price">$50</h3>
+    <div class="content">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half"></i>
+        </div>
+        <a href="#" class="title">Cyber Security</a>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, beatae. Modi quos excepturi id quibusdam? Molestiae quis nihil non debitis!</p>
+        <div class="info">
+            <h3> <i class="far fa-clock"></i> 2 hours </h3>
+            <h3> <i class="far fa-calendar-alt"></i> 6 months </h3>
+            <h3> <i class="fas fa-book"></i> 12 modules </h3>
+        </div>
+    </div>
+</div>
+
+
+
+    
+
+<div class="box">
+    <img src="images/javascript.jpeg" alt="">
+    <h3 class="price">$50</h3>
+    <div class="content">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half"></i>
+        </div>
+        <a href="#" class="title">Javascript</a>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, beatae. Modi quos excepturi id quibusdam? Molestiae quis nihil non debitis!</p>
+        <div class="info">
+            <h3> <i class="far fa-clock"></i> 2 hours </h3>
+            <h3> <i class="far fa-calendar-alt"></i> 6 months </h3>
+            <h3> <i class="fas fa-book"></i> 12 modules </h3>
+        </div>
+    </div>
+</div>
+
+</div>
+
+<!-- contact section starts  -->
+
+<section class="contact" id="contact">
+
+<h1 class="heading">Contact Us</h1>
+
+<div class="row">
+
+    <form action="">
+        <input type="text" placeholder="full name" class="box">
+        <input type="email" placeholder="your email" class="box">
+        <input type="password" placeholder="your password" class="box">
+        <input type="number" placeholder="your number" class="box">
+        <textarea name="" id="" cols="30" rows="10" class="box address" placeholder="your address"></textarea>
+        <input type="submit" class="btn" value="send now">
+    </form>
+
+    <div class="image">
+        <img src="images/contact-img.png" alt="">
+    </div>
+
+</div>
+
+</section>
+
+<!-- contact section ends -->
+
+
+
+        <div class="footer">
+			<div class="container">
+				<div class="col-md-3 ftr_navi ftr">
+					<h3>NAVIGATION</h3>
+					<ul>
+						<li>
+							<a href="../Homepage/index.php">Home</a>
+						</li>
+						<li>
+							<a href="../Profilers/SProfile/index.php">Student Login</a>
+						</li>
+						<li>
+							<a href="../Profilers/PProfile/index.php">Placement Login</a>
+						</li>
+						<li>
+							<a href="../Profilers/HODProfile/index.php">HOD Login</a>
+						</li>
+						<li>
+							<a href="../Profilers/Priprofile/index.php">Administrative Login</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-3 ftr_navi ftr">
+					<h3>MEMBERS</h3>
+					<ul>
+						<li>
+							<a href="#">Customer Support</a>
+						</li>
+						<li>
+							<a href="#">Placement Support</a>
+						</li>
+						<li>
+							<a href="#">Faculty Support</a>
+						</li>
+						<li>
+							<a href="#">Registered Companies</a>
+						</li>
+						<li>
+							<a href="#">Training</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-3 get_in_touch ftr">
+					<h3>GET IN TOUCH</h3>
+					
+					<p>Mumbai, India</p>
+					<p>08138-223818/223365</p>
+					<a href="mailto:cittumkur@rediffmail.com">office@xavierengg.com</a>
+				</div>
+				<div class="col-md-3 ftr-logo">
+					<p>Copyright &copy; 2021  | Developed by
+              <a href="" target="_parent">Xavierites</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+    </body>
+
+</html>
