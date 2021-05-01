8f606e7d178147ed2f16af9d8bf4e5d392baaaef
bethelthomas <bethelthomas55@gmail.com>
2021-05-01 14:20:12 +0530

    Update login1.php

--- Profilers/SProfile/login1.php
+++ Profilers/SProfile/login1.php
@@ -5,7 +5,7 @@
 	
 	if ($username&&$password)
 	{
-		$mysqli = new mysqli('mysql5.gear.host','details4','Xie@campus','details4');
+		$mysqli = new mysqli('ftp.gear.host','xiecampusautomation\$xiecampusautomation','LDiqxnY6DjjPENkpZKLc4fbYsfmFWb9jMdZKwQxkdgYeoi63Z6G61zoJEi1W','details4');
 		if($mysqli->connect_error) { die('Error'.('.$mysqli->connect_errno.').'$mysqli->connect_error');}
 		else
 		{
