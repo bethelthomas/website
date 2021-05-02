2641bc8fc48a5d970f42890ea0a0ccdcb87598a9
bethelthomas <65564630+bethelthomas@users.noreply.github.com>
2021-05-02 14:51:33 +0530

    Update rs1.php

--- Profilers/SProfile/rs1.php
+++ Profilers/SProfile/rs1.php
@@ -7,12 +7,13 @@
 	$USN2 = ($_SESSION['reset']);
 	
 	$connect = 
-	$mysqli = new mysqli('localhost','root','','details');
-	if($mysqli->connect_error) { die('Error'.('.$mysqli->connect_errno.').'$mysqli->connect_error');}
-	else
-	{
-		echo "Connected to database";
-	}
+		$mysqli = new mysqli('remotemysql.com','NG73FMUEBv','AOMDJxJRXe','NG73FMUEBv');
+		
+		if($mysqli->connect_error) { die('Error'.('.$mysqli->connect_errno.').'$mysqli->connect_error');}
+		else
+		{
+			echo "";
+		}
 	
 			if($USN1 && $password && $confirm ){
 		
