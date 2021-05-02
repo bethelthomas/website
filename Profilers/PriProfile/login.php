6b2d14c1f66796c33d698ef9fbf5235cbbbc3189
bethelthomas <65564630+bethelthomas@users.noreply.github.com>
2021-05-02 14:41:43 +0530

    Update login.php

--- Profilers/PriProfile/login.php
+++ Profilers/PriProfile/login.php
@@ -152,13 +152,13 @@
                     <h2 class="media-heading text-uppercase">Drive Results</h2>
                     <p>Latest Drive Result Overview</p> 
                     <?php	
-                    
-$mysqli = new mysqli('localhost','root','','details');
-if($mysqli->connect_error) { die('Error'.('.$mysqli->connect_errno.').'$mysqli->connect_error');}
-else
-{
-	echo "Connected to database";
-}
+ $mysqli = new mysqli('remotemysql.com','NG73FMUEBv','AOMDJxJRXe','NG73FMUEBv');
+		
+		if($mysqli->connect_error) { die('Error'.('.$mysqli->connect_errno.').'$mysqli->connect_error');}
+		else
+		{
+			echo "";
+		}
 $RESULT=mysqli_query($mysqli,"SELECT DISTINCT count(CompanyName) from addpdrive where PVenue LIKE '%CIT%' AND YEAR(Date)=YEAR(NOW())");
 $data=mysqli_fetch_assoc($RESULT);
 echo "<br><br><h3>Companies In Our Campus In This Year&nbsp:&nbsp";
