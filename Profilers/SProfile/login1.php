7851677c74c88677ecc4e32e90ba97b588ab3fb3
bethelthomas <65564630+bethelthomas@users.noreply.github.com>
2021-05-02 13:47:56 +0530

    Update login1.php

--- Profilers/SProfile/login1.php
+++ Profilers/SProfile/login1.php
@@ -5,10 +5,9 @@
 	
 	if ($username&&$password)
 	{
-		//$mysqli = new mysqli('den1.mysql5.gear.host','details4','Xie@campus');
-		//remote
+		
 		$mysqli = new mysqli('remotemysql.com','NG73FMUEBv','AOMDJxJRXe','NG73FMUEBv');
-		//'localhost','root','','details'
+		
 		if($mysqli->connect_error) { die('Error'.('.$mysqli->connect_errno.').'$mysqli->connect_error');}
 		else
 		{
