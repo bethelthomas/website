1b29554dd71ccb736cd35fcc3211587f098920b2
bethelthomas <65564630+bethelthomas@users.noreply.github.com>
2021-05-02 14:26:08 +0530

    Update rs1.php

--- Profilers/PProfile/rs1.php
+++ Profilers/PProfile/rs1.php
@@ -4,13 +4,14 @@
 	$USN1 = $_POST['USN'];
 	$password = $_POST['PASSWORD'];
 	$confirm = $_POST['repassword'];
+	 $mysqli = new mysqli('remotemysql.com','NG73FMUEBv','AOMDJxJRXe','NG73FMUEBv');
+		
+		if($mysqli->connect_error) { die('Error'.('.$mysqli->connect_errno.').'$mysqli->connect_error');}
+		else
+		{
+			echo "";
+		}
 	
-$mysqli = new mysqli('localhost','root','','details');
-if($mysqli->connect_error) { die('Error'.('.$mysqli->connect_errno.').'$mysqli->connect_error');}
-else
-{
-	echo "Connected to database";
-}
 	
 	if($password == $confirm) {
 		if($sql = mysqli_query($mysqli,"UPDATE `placement`.`plogin` SET `Password` ='$password' WHERE `plogin`.`Username` = '$USN1'"));
