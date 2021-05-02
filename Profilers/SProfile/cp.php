dfacd7c11201ecf3a1fa6e708df85a1a2fe53b48
bethelthomas <65564630+bethelthomas@users.noreply.github.com>
2021-05-02 14:48:15 +0530

    Update cp.php

--- Profilers/SProfile/cp.php
+++ Profilers/SProfile/cp.php
@@ -1,12 +1,13 @@
 <?php
 session_start();
 $connect = 
-$mysqli = new mysqli('localhost','root','','details');
-if($mysqli->connect_error) { die('Error'.('.$mysqli->connect_errno.').'$mysqli->connect_error');}
-else
-{
-	echo "Connected to database";
-}
+	$mysqli = new mysqli('remotemysql.com','NG73FMUEBv','AOMDJxJRXe','NG73FMUEBv');
+		
+		if($mysqli->connect_error) { die('Error'.('.$mysqli->connect_errno.').'$mysqli->connect_error');}
+		else
+		{
+			echo "";
+		}
 
 	$Username = $_SESSION['username'];
 	$Password = $_POST['Password'];
