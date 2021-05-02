92b204c9daa9a4f0bad94dd95425ef6dee66e2ae
bethelthomas <65564630+bethelthomas@users.noreply.github.com>
2021-05-02 14:44:33 +0530

    Update rs1.php

--- Profilers/PriProfile/rs1.php
+++ Profilers/PriProfile/rs1.php
@@ -4,11 +4,12 @@
 	$USN1 = $_POST['USN'];
 	$password = $_POST['PASSWORD'];
 	$confirm = $_POST['repassword'];
-	$mysqli = new mysqli('localhost','root','','details');
+$mysqli = new mysqli('remotemysql.com','NG73FMUEBv','AOMDJxJRXe','NG73FMUEBv');
+		
 		if($mysqli->connect_error) { die('Error'.('.$mysqli->connect_errno.').'$mysqli->connect_error');}
 		else
 		{
-			echo "Connected to database";
+			echo "";
 		}
 
 	
