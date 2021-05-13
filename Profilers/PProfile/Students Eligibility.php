19cc0c5e91955c78d1e6e5eb7c7902e1e56a4db6
bethelthomas <65564630+bethelthomas@users.noreply.github.com>
2021-05-14 02:28:43 +0530

    Update Students Eligibility.php

--- Profilers/PProfile/Students Eligibility.php
+++ Profilers/PProfile/Students Eligibility.php
@@ -14,7 +14,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">  
-    <title>Principal - Student Details</title>
+    <title>Drive Details</title>
     <meta name="description" content="">
     <meta name="author" content="templatemo">    
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,700' rel='stylesheet' type='text/css'>
