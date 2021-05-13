451857f1a81d0ff5c0e036e7f939651ce4b5ce40
bethelthomas <65564630+bethelthomas@users.noreply.github.com>
2021-05-14 01:28:05 +0530

    Update add pdrive1.php

--- Profilers/PProfile/Placement Drives/add pdrive1.php
+++ Profilers/PProfile/Placement Drives/add pdrive1.php
@@ -1,10 +1,11 @@
 <?php
-$mysqli = new mysqli('localhost','root','','details');
-if($mysqli->connect_error) { die('Error'.('.$mysqli->connect_errno.').'$mysqli->connect_error');}
-else
-{
-	echo "Connected to database";
-}
+$mysqli = new mysqli('remotemysql.com','NG73FMUEBv','AOMDJxJRXe','NG73FMUEBv');
+		
+		if($mysqli->connect_error) { die('Error'.('.$mysqli->connect_errno.').'$mysqli->connect_error');}
+		else
+		{
+			echo "";
+		}
 if(isset($_POST['submit']))
 { 
 $cname = $_POST['compny'];
